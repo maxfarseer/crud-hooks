b9be15b4192089e1fdd402e7d3c5bbaa4ecaf767
Max P <maxfarseer@gmail.com>
2019-01-08 14:24:06 +0300

    count example

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <p>you clicked {count}</p>
+          <button onClick={() => setCount(count + 1)}>Click me</button>
         </div>
         <div className="flex-large">
           <h2>View users</h2>
