2416f2b404a7d35b48264206016f43b21e3215ca
Max P <maxfarseer@gmail.com>
2019-01-17 07:36:34 +0300

    change files name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { AddUserForm } from './forms/AddUserForm_'
+import { AddUserForm } from './forms/AddUserForm'
 import { EditUserForm } from './forms/EditUserForm'
-import { UserTable } from './tables/UserTable_'
+import { UserTable } from './tables/UserTable'
 import './App.css'
 
 const App = () => {
